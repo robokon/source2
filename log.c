5fcc1568d9c2fdc88481c0c2e05e8f7e91262e3b
tomoki-kojima <robokon_ksk@outlook.com>
2017-08-18 14:41:39 +0900

    ログの保持およびコミットの方法を修正

--- log.c
+++ log.c
@@ -1,7 +1,9 @@
 #include "log.h"
 #include "Distance.h"
 
-#define  LOG_MAX   10000                  /* Log の最大回数 */
+#define  LOG_MAX   15000                  /* Log の最大回数 */
+#define  LOG_COMMIT_INTERVAL 2500
+
 #define  LOG_FILE_NAME  "Log_%08d.csv" /* Log fileの名前 */
 
 /* Log用の構造体 */
@@ -13,7 +15,11 @@ typedef struct{
     int16_t Distance;
 }Logger;
 
-int LogNum = 0;              /* Log回数の格納変数 */
+typedef enum { NON_HEADER, INCLUDE_HEADER } CommitStyle;
+
+static int LogNum = 0;              /* Log格納配列の現在位置 */
+static int oldLogNum = 0;           /* Log格納配列の書き込み済位置 */
+static int16_t count = 0;               /* Logのカウント */
 Logger gst_Log_str[LOG_MAX]; /* Log格納配列 */
 void logStr(uint8_t reflect, int16_t p, int16_t i, int16_t d, int16_t);
 
@@ -36,7 +42,8 @@ void log_Str(uint8_t reflect, int16_t p, int16_t i, int16_t d)
         gst_Log_str[LogNum].I = i;
         gst_Log_str[LogNum].D = d;
         gst_Log_str[LogNum].Distance = Distance_getDistance();
-        LogNum++;    
+        LogNum++;
+        LogNum %= LOG_MAX;    
     }
 }
 
@@ -47,17 +54,26 @@ void log_Str(uint8_t reflect, int16_t p, int16_t i, int16_t d)
 // 概要 : グローバル配列 gst_Log_strに格納されているデータをファイル出力する
 //
 //*****************************************************************************
-static void write_data(FILE *fp)
+static void write_data(FILE *fp[], int fpsize, CommitStyle cstyle)
 {
-    int  i;   /* インクリメント */
+    int  i, j;   /* インクリメント */
 
     /* 列タイトル挿入 */
-    fprintf(fp,"反射光センサー, P,I,D,走行距離　\n");
+    if (cstyle == INCLUDE_HEADER) {
+        for(j = 0; j < fpsize; j++) {
+            fprintf(fp[j],"count,reflectedLight,P,I,D,distance\n");
+        }
+    }
     
     /* Logデータの出力 */
-    for(i = 0 ; i < LogNum; i++)
+    if (oldLogNum == LogNum)
+        return;
+
+    for(i = oldLogNum, oldLogNum = LogNum; i != (oldLogNum + 1) % LOG_MAX; i = (i + 1) % LOG_MAX, count++)
     {
-        fprintf(fp,"%d,%d,%d,%d,%d\n",gst_Log_str[i].Reflect, gst_Log_str[i].P, gst_Log_str[i].I, gst_Log_str[i].D, gst_Log_str[i].Distance);
+        for(j = 0; j < fpsize; j++) {
+            fprintf(fp[j], "%d,%d,%d,%d,%d,%d\n", count, gst_Log_str[i].Reflect, gst_Log_str[i].P, gst_Log_str[i].I, gst_Log_str[i].D, gst_Log_str[i].Distance);
+        }
     }
 }
 
@@ -68,13 +84,19 @@ static void write_data(FILE *fp)
 // 概要 : ローカルファイルおよびbluetooth接続先にログのデータを出力する
 //
 //*****************************************************************************
-void log_Commit()
+void log_Commit(CommitStyle cstyle)
 {
     FILE *localfile; /* ファイルポインタ */
-    localfile = fopen(logfile_name, "a");
+    FILE *fp[2];
 
-    write_data(localfile);
-    write_data(bluetooth);
+    localfile = fopen(logfile_name, "a");
+    if (localfile == NULL)
+        fprintf(bluetooth, "localfile open error! %s can not open.", logfile_name);
+
+    fp[0] = bluetooth;
+    fp[1] = localfile;
+
+    write_data(fp, 2, cstyle);
 
     fclose(localfile);
 }
@@ -96,6 +118,7 @@ void initialize_log(FILE *_bluetooth)
         i++;
     } while (fopen(logfile_name, "r") != NULL);
 
+    log_Commit(INCLUDE_HEADER);
     act_tsk(LOG_TASK);
 }
 
@@ -109,7 +132,7 @@ void initialize_log(FILE *_bluetooth)
 void close_log()
 {
     ter_tsk(LOG_TASK);
-    log_Commit();
+    log_Commit(NON_HEADER);
 }
 
 //*****************************************************************************
@@ -121,11 +144,13 @@ void close_log()
 //*****************************************************************************
 void log_task(intptr_t unused)
 {
-    if (LogNum > (LOG_MAX / 2)) {
-        log_Commit();
-        LogNum = 0;
+    while(1) {
+        if (LogNum > LOG_COMMIT_INTERVAL) {
+            log_Commit(NON_HEADER);
+            LogNum = 0;
+        }
+        tslp_tsk(10); /* 10msecウェイト */
     }
-    tslp_tsk(10); /* 10msecウェイト */
 }
 
 /* end of file */
--- log.h
+++ log.h
@@ -12,7 +12,6 @@ extern "C" {
 #include "common.h"
 
 extern void log_Str(uint8_t reflect, int16_t p, int16_t i, int16_t d);
-extern void log_Commit();
 extern void initialize_log(FILE *_bluetooth);
 extern void close_log();
 extern void log_task(intptr_t unused);
